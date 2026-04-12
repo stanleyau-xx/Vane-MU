43c0ab5101c7db3cb511f71ebdcebe821014e92c
Stanley Au <stanleyau@hknetworks.com>
2026-04-12 21:25:19 +0000

    fix: revert darkMode change (caused theme styling issues)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: ['class', '.warm'],
+  darkMode: 'class',
   theme: {
     extend: {
       borderColor: ({ colors }) => {
